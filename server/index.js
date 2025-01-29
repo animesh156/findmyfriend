8e5fda92b58f0e7d286e2351a243b1424b5ec9cb
animesh <animeshrathore255@gmail.com>
2025-01-29 18:08:27 +0530

    created backend folder & installed corresponding dependancies

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv').config()
+const port = process.env.PORT 
+
+
+app.get('/', (req,res) => {
+    res.send('Hello World')
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
