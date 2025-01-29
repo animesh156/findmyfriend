0ea9c68e55b60502c16dc95b3d82dff38bf30baf
animesh <animeshrathore255@gmail.com>
2025-01-29 18:20:31 +0530

    created user model & connected backend to database using mongo uri

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
+const cors = require('cors')
+const connectDB = require('./config/db')
 const port = process.env.PORT 
+
+
+connectDB()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 
 app.get('/', (req,res) => {
